5c42d05b9588b3f26f59c66f3a14fd20e685eb8a
TomasNagy <tomi.nagy@outlook.com>
2023-05-08 15:10:04 +0200

    Implement command line arguments

--- commandLineArgument.c
+++ commandLineArgument.c
@@ -1,9 +1,18 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void)
+int main(int argc, string argv[])
 {
-	string name = get_string("What's your name? ");
-
-	printf("Hello, %s!\n", name);
+	if (argc == 2)
+	{
+		printf("Hello, %s!\n", argv[1]);
+	}
+	else if (argc == 3)
+	{
+		printf("Hello, %s %s!\n", argv[1], argv[2]);
+	}	
+	else
+	{
+		printf("Hello, guest!\n");
+	}
 }
